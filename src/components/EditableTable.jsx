ce31eb54049099a79b1ac9acf72504a07451cf77
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-21 02:45:10 -0700

    Add basic layout to EditableTable component
    Introduced initial structure and styling for the EditableTable component, including a gradient header and responsive flex layout. This sets up the foundation for further table functionality.

--- src/components/EditableTable.jsx
+++ src/components/EditableTable.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const EditableTable = () => {
   return (
-    <div>EditableTable</div>
-  )
-}
+    <div className="bg-white rounded-2xl shadow-2xl overflow-hidden border boarder boarder-r-gray-100">
+      <div className="bg-gradient-to-r from from-indigo-600 via-purple-600 to-indigo-700 px-8 py-6 ">
+        <div className="flex flex-cols lg:flex-row lg:items-center lg:justify-between space-y-4 lg:space-y-0">
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default EditableTable
+export default EditableTable;

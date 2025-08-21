5fb5a32ed6b50539eb953229ea8cfab34bd2ed79
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-20 18:17:21 -0700

    Add EditableTable component to App
    Imported and rendered the EditableTable component within the App layout to display the editable table in the main application view.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
+import EditableTable from './components/EditableTable';
 
 const App = () => {
   return (
-    <div className='min-h-screen'>
-      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 '>
-        <div className='space-y-8 '>
-          
-</div>
+    <div className="min-h-screen">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 ">
+        <div className="space-y-8 ">
+          <EditableTable />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

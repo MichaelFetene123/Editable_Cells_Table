36765360e32c5e87972379c46345295cb38b60dc
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-20 18:22:59 -0700

    Add technical details section to App
    Introduced a new styled section labeled 'Technical implements' below the EditableTable component for displaying technical details.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ const App = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 ">
         <div className="space-y-8 ">
           <EditableTable />
+          {/* technical details  */}
+          <div className="bg-gradient-to-r from-gray-900 to-gray-800  rounded-2xl p-8 text-white">
+<h3 className="text-2xl font-bold mb-6 text-center">Technical implements</h3>
+          </div>
         </div>
       </div>
     </div>

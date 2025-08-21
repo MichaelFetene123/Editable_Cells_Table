2a849b0b8d4fb8f441f1881891e37502d4fd9d03
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-20 18:09:44 -0700

    Add layout containers to App component
    Introduced max-width, padding, and spacing containers to the App component for improved layout structure and responsiveness.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='min-h-screen'>App</div>
+    <div className='min-h-screen'>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 '>
+        <div className='space-y-8 '>
+          
+</div>
+      </div>
+    </div>
   )
 }
 
